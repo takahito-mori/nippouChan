034568c23464ae83a6f1fbe9a7f30938b5816334
C1534 <C1534@BSP01164.bsp.co.jp>
2017-08-21 10:10:39 +0900

    NippouServiceの追加

--- src/main/java/jp/co/unirita/dairyreport/domain/nippou/NippouRepository.java
+++ src/main/java/jp/co/unirita/dairyreport/domain/nippou/NippouRepository.java
@@ -1,10 +1,13 @@
 package jp.co.unirita.dairyreport.domain.nippou;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NippouRepository extends CrudRepository<Nippou, String> {
+    List<Nippou> findByNippouId(int nippouId);
 
 }
 
