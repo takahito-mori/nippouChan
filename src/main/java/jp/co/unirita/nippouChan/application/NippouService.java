0192276fede9b294f9a0273e3b00691232bc5841
C1534 <C1534@BSP01164.bsp.co.jp>
2017-08-22 17:33:40 +0900

    NippouServiceのへんこう

--- src/main/java/jp/co/unirita/nippouChan/application/NippouService.java
+++ src/main/java/jp/co/unirita/nippouChan/application/NippouService.java
@@ -1,24 +1,19 @@
 package jp.co.unirita.nippouChan.application;
 
 import java.sql.Timestamp;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import jp.co.unirita.nippouChan.domain.nippou.CreateComparator;
 import jp.co.unirita.nippouChan.domain.nippou.Nippou;
 import jp.co.unirita.nippouChan.domain.nippou.NippouRepository;
-import jp.co.unirita.nippouChan.domain.nippou.TrainingComparator;
 import jp.co.unirita.nippouChan.domain.user.User;
 
 @Service
 public class NippouService {
     @Autowired
     NippouRepository nippouRepository;
-    private static final AtomicInteger count = new AtomicInteger(0);
 
     /**
      * (サンプル実装)
@@ -32,8 +27,7 @@ public class NippouService {
         user.setUserId("00001");
         nippou.setNippouRegister(new Timestamp(System.currentTimeMillis()));
         nippou.setNippouEdit(new Timestamp(System.currentTimeMillis()));
-        nippou.setNippouId(count.incrementAndGet());
- //       nippou.setNippouId(nippou);
+		nippou.setNippouId(getListSize()+1); //       nippou.setNippouId(nippou);
 //        nippou.setUser(user); // TODO: 現在のログインユーザを取得して設定するようにしたい
 //        nippou.setCreated(new Date());
         nippouRepository.save(nippou);
@@ -48,14 +42,14 @@ public class NippouService {
 
     }
 
-    /**
-     * (サンプル実装)
-     * userIdをキーにしてDBから日報のリストを取得して返す
-     * @param nippouId
-     * @return List<Nippou>
-     */
-    public List<Nippou> getListByNippouId(int nippouId) {
-        return nippouRepository.findByNippouId(nippouId);
+	/**
+	 * (サンプル実装)
+	 * userをキーにしてDBから日報のリストを取得して返す
+	 * @param user
+	 * @return List<Nippou>
+	 */
+/*    public List<Nippou> getListByUserId(User user) {
+        return nippouRepository.findByUser(user);
     }
 
     /**
@@ -74,28 +68,34 @@ public class NippouService {
 		return nippouRepository.findAll();
 	}
 
+	public int getListSize() {
+		List<Nippou> list=getAll();
+		return list.size();
+	}
 
 
+//下はjsでソートするから意味ないですね…
     /**
      * nippouIdをキーにしてDBから日報を取得し、作成日時の降順(最新)でソートして返す
      * @param nippouId
      * @return List<Nippou>
      */
+    /*
     public List<Nippou> getListByNewest(int nippouId) {
     	List<Nippou> nippouList = nippouRepository.findByNippouId(nippouId);
     	Collections.sort(nippouList, new CreateComparator());
     	return nippouList;
     }
-
+*/
     /**
      * nippouIdをキーにしてDBから日報を取得し、研修日時の降順(最新)でソートして返す
      * @param nippouId
      * @return List<Nippou>
-     */
+     /*
     public List<Nippou> getListByTraining(int nippouId) {
     	List<Nippou> nippouList = nippouRepository.findByNippouId(nippouId);
     	Collections.sort(nippouList, new TrainingComparator());
     	return nippouList;
     }
-
+*/
 }
