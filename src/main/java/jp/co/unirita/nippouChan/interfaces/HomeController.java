e0d679e5d7768a79025324d3d9a2d9ef7a98ff02
BSP\C1542 <cds2017c@unilita.co.jp>
2017-08-24 11:12:30 +0900

    ユーザーIDの照合機能の追加

--- src/main/java/jp/co/unirita/nippouChan/interfaces/HomeController.java
+++ src/main/java/jp/co/unirita/nippouChan/interfaces/HomeController.java
@@ -3,13 +3,15 @@ package jp.co.unirita.nippouChan.interfaces;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.unirita.nippouChan.application.NippouService;
+import jp.co.unirita.nippouChan.application.user.NippouChanUserDetails;
 import jp.co.unirita.nippouChan.domain.nippou.Nippou;
-
+import jp.co.unirita.nippouChan.domain.user.User;
 @Controller
 public class HomeController {
     @Autowired
@@ -22,11 +24,14 @@ public class HomeController {
      */
 
     @GetMapping("/home")
-    public ModelAndView home(Nippou nippou) {
+    public ModelAndView home(Nippou nippou,@AuthenticationPrincipal NippouChanUserDetails userDetails) {
       List<Nippou> report = nippouService.getAll();
+      User user = userDetails.getUser();
+
     	        ModelAndView mav = new ModelAndView("home_page");
 
         mav.addObject("nippou", report);
+        mav.addObject("loginuser",user);
         return mav;
     }
 
--- src/main/java/jp/co/unirita/nippouChan/interfaces/NippouController.java
+++ src/main/java/jp/co/unirita/nippouChan/interfaces/NippouController.java
@@ -3,6 +3,7 @@ package jp.co.unirita.nippouChan.interfaces;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.unirita.nippouChan.application.NippouService;
+import jp.co.unirita.nippouChan.application.user.NippouChanUserDetails;
 import jp.co.unirita.nippouChan.domain.nippou.Nippou;
+import jp.co.unirita.nippouChan.domain.user.User;
 
 @Controller
 @RequestMapping("/nippou")
@@ -29,10 +32,20 @@ public class NippouController {
      */
 
     @RequestMapping("/{id}")
-    public ModelAndView showOne(@PathVariable("id") int nippouId) {
+    public ModelAndView showOne(@PathVariable("id") int nippouId,@AuthenticationPrincipal NippouChanUserDetails userDetails) {
         Nippou nippou = nippouService.getOne(nippouId);
+        User user = userDetails.getUser();
         ModelAndView mav = new ModelAndView("show_page");
         mav.addObject("nippou", nippou);
+        mav.addObject("loginuser",user);
+        int flag = 0;
+        System.out.println(nippou.getUser().getUserId());
+        System.out.println(user.getUserId());
+
+        if(user.getUserId().toString().equals(nippou.getUser().getUserId().toString())) {
+        	flag = 1;
+        }
+        mav.addObject("flag",flag);
         return mav;
     }
 
@@ -44,11 +57,13 @@ public class NippouController {
      * @return
      */
     @PostMapping
-    public ModelAndView create(@Validated Nippou nippou, BindingResult result) {
+    public ModelAndView create(@Validated Nippou nippou, BindingResult result,@AuthenticationPrincipal NippouChanUserDetails userDetails) {
         nippouService.create(nippou);
         List<Nippou> report = nippouService.getAll();
+        User user = userDetails.getUser();
         ModelAndView mav = new ModelAndView("home_page");
         mav.addObject("nippou", report);
+        mav.addObject("loginuser",user);
 
         return mav;
     }
